ee2d726ae84b0a27e2c2665c765bc01ae0cde31a
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-05-19 14:48:00 -0400

    updated for online hosting

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,13 +1,2 @@
-let APIURL = '';
-
-switch (window.location.hostname) {
-    case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:3000';
-        break;
-
-        case 'gamer-genus-engelken.herokuapp.com':
-
-        APIURL = 'https://gamer-genus.herokuapp.com'
-}
-
-export default APIURL
+let APIURL = 'https://gamer-genus-backend-3.onrender.com';
+export default APIURL;
